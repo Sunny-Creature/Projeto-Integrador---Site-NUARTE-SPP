e1adc23258859fd56ea59f203c1592ff6da87770
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-11-24 10:28:54 -0300

    first commit

---
+++ nuarte/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+# class Grupos(models.Model):
+#    nome_grupo = models.CharField(max_lenght=10)
+#    icone_grupo = models.ImageField
+
+#class Orientadores(models.Model):
+#    nome_orientador = models.CharField(max_length=20)
+#    icone_orientador = models.ImageField
---
+++ nuarte/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('events/', views.eventos, name ="eventos"),
+    path('groups/', views.grupos, name="grupos"),
+    path('history/', views.historia, name="historia"),
+]
