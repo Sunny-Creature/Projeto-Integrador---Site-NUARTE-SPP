c8f79e980b8e5714f5eb358488afa5eb0afc7497
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-11-26 12:01:39 -0300

    migrations

--- nuarte/forms.py
+++ nuarte/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class FormContato(forms.form):
+class FormContato(forms.Form):
     nome = forms.CharField(label="Nome:")
     email = forms.EmailField(label="E-mail:")
-    mensagem = forms.CharField(widget=forms.Textarea, max_lenght="250", label="Mensagem:")
+    mensagem = forms.CharField(widget=forms.Textarea, max_length="250", label="Mensagem:")
--- nuarte/urls.py
+++ nuarte/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('events/', views.eventos, name ="eventos"),
-    path('groups/', views.grupos, name="grupos"),
-    path('history/', views.historia, name="historia"),
 ]
